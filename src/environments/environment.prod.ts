2b661330a203f490a61a7613c51005a96bf0a7e9
Amila Kularatne (WLT GB) <amila.kularatne@Wlt.com>
2019-07-11 14:29:25 +0100

    init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAi46B_wpVEiyw9XwPgJX5pawiej71I3Ck",
+    authDomain: "crit-dd1d6.firebaseapp.com",
+    databaseURL: "https://crit-dd1d6.firebaseio.com",
+    projectId: "crit-dd1d6",
+    storageBucket: "",
+    messagingSenderId: "199337054995",
+    appId: "1:199337054995:web:b9f5ae03999dc3f6"
+  },
+};
